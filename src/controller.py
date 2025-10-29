bc99eefdebbeac75bcd0f5ad510d48662e3ae13f
Bruno Krieg <bruno.krieg.work@gmail.com>
2025-10-29 07:22:46 +0100

    switch gpiozero

--- src/controller.py
+++ src/controller.py
@@ -1,13 +1,9 @@
 import os
 import platform
-from src.mock_controller import MockVentilationController
+from gpiozero import OutputDevice
+from gpiozero.pins.mock import MockFactory
+from gpiozero.exc import BadPinFactory
 from src.models import VentilationLevel
-
-try:
-    import RPi.GPIO as GPIO
-    is_raspberry_pi = True
-except (ImportError, RuntimeError):
-    is_raspberry_pi = False
 
 # Environment variable to force mock mode
 FORCE_MOCK_ENV = "VENTILATION_FORCE_MOCK"
@@ -19,54 +15,60 @@ class VentilationController:
     PIN_27 = 27
     
     def __init__(self):
-        use_mock = not is_raspberry_pi or os.getenv(FORCE_MOCK_ENV, "false").lower() == "true"
+        """Initialize the ventilation controller with real or mock GPIO"""
+        use_mock = os.getenv(FORCE_MOCK_ENV, "false").lower() == "true"
         
-        if use_mock:
-            self._controller = MockVentilationController()
-        else:
-            self._controller = self._setup_gpio_controller()
+        try:
+            if use_mock:
+                from gpiozero.pins.mock import MockFactory
+                from gpiozero import Device
+                Device.pin_factory = MockFactory()
+                print("Running in mock mode - no GPIO control active")
+            
+            # Initialize GPIO outputs
+            self._pin_17 = OutputDevice(self.PIN_17, active_high=True, initial_value=False)
+            self._pin_22 = OutputDevice(self.PIN_22, active_high=True, initial_value=False)
+            self._pin_27 = OutputDevice(self.PIN_27, active_high=True, initial_value=False)
+            self._current_level = VentilationLevel.OFF
+            
+        except BadPinFactory:
+            # If we can't initialize real GPIO and mock wasn't requested, force mock mode
+            from gpiozero.pins.mock import MockFactory
+            from gpiozero import Device
+            Device.pin_factory = MockFactory()
+            print("No GPIO hardware detected, falling back to mock mode")
+            self._pin_17 = OutputDevice(self.PIN_17, active_high=True, initial_value=False)
+            self._pin_22 = OutputDevice(self.PIN_22, active_high=True, initial_value=False)
+            self._pin_27 = OutputDevice(self.PIN_27, active_high=True, initial_value=False)
+            self._current_level = VentilationLevel.OFF
     
-    def _setup_gpio_controller(self):
-        """Set up the real GPIO controller"""
-        class GPIOController:
-            def __init__(self, pin_17, pin_22, pin_27):
-                self.PIN_17 = pin_17
-                self.PIN_22 = pin_22
-                self.PIN_27 = pin_27
-                self.current_level = VentilationLevel.OFF
-                
-                # Set up GPIO
-                GPIO.setmode(GPIO.BCM)
-                GPIO.setup(self.PIN_17, GPIO.OUT)
-                GPIO.setup(self.PIN_22, GPIO.OUT)
-                GPIO.setup(self.PIN_27, GPIO.OUT)
-            
-            def set_level(self, level: VentilationLevel):
-                """Set the ventilation level using real GPIO"""
-                self.current_level = level
-                
-                if level == VentilationLevel.OFF:
-                    self._set_gpio_state(True, False, False)  # PIN 17 high, others low
-                elif level == VentilationLevel.LOW:
-                    self._set_gpio_state(False, False, False)  # All pins low
-                elif level == VentilationLevel.MEDIUM:
-                    self._set_gpio_state(False, False, True)  # PIN 27 high, others low
-                elif level == VentilationLevel.HIGH:
-                    self._set_gpio_state(False, True, False)  # PIN 22 high, others low
-            
-            def get_level(self) -> VentilationLevel:
-                """Get the current ventilation level"""
-                return self.current_level
-            
-            def cleanup(self):
-                """Cleanup GPIO resources"""
-                GPIO.cleanup()
-            
-            def _set_gpio_state(self, pin_17_state: bool, pin_22_state: bool, pin_27_state: bool):
-                """Set the GPIO pins to the specified states"""
-                GPIO.output(self.PIN_17, GPIO.HIGH if pin_17_state else GPIO.LOW)
-                GPIO.output(self.PIN_22, GPIO.HIGH if pin_22_state else GPIO.LOW)
-                GPIO.output(self.PIN_27, GPIO.HIGH if pin_27_state else GPIO.LOW)
+    def set_level(self, level: VentilationLevel):
+        """Set the ventilation level"""
+        self._current_level = level
+        
+        # Reset all pins to low first
+        self._pin_17.off()
+        self._pin_22.off()
+        self._pin_27.off()
+        
+        # Set the appropriate pin high based on the level
+        if level == VentilationLevel.OFF:
+            self._pin_17.on()  # PIN 17 high, others low
+        elif level == VentilationLevel.MEDIUM:
+            self._pin_27.on()  # PIN 27 high, others low
+        elif level == VentilationLevel.HIGH:
+            self._pin_22.on()  # PIN 22 high, others low
+        # For LOW level, all pins remain low
+    
+    def get_level(self) -> VentilationLevel:
+        """Get the current ventilation level"""
+        return self._current_level
+    
+    def cleanup(self):
+        """Cleanup GPIO resources"""
+        self._pin_17.close()
+        self._pin_22.close()
+        self._pin_27.close()
         
         return GPIOController(self.PIN_17, self.PIN_22, self.PIN_27)
     
